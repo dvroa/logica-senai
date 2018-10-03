285ef3c42e0edba5ffdf43733c39145ae9343322
jlog180924 <jlog180924@info.local>
2018-10-02 22:01:05 -0300

    aula7

--- aulas/workspace/loops-java/src/exemplos/DoWhile.java
+++ aulas/workspace/loops-java/src/exemplos/DoWhile.java
@@ -2,7 +2,14 @@ package exemplos;
 
 public class DoWhile {
 	public static void main(String[] args) {
+		int num = 1;
 		
+		do 
+		{
+			System.out.println("Olha eu aqui pela " + num + "ª vez!");
+			++num;
+		} 
+		while (num < 10);
 	}
 
 }
